1033f2320a31b39ad5f6a23b954616d9482cdef8
Ricky Chilcott <ricky@rickychilcott.com>
2012-04-17 13:31:20 -0400

    Adding a flag to compile assets if not found

--- config/application.rb
+++ config/application.rb
@@ -17,6 +17,7 @@ module Munki
     
     # Enable the asset pipeline
     config.assets.enabled = true
+    config.assets.compile = true
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
